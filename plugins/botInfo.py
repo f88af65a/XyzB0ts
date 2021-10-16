01ed76397f4ab1f331c8a7570c095515f0d11de1
MN1XYZ <179180144@qq.com>
2021-10-16 21:33:46 +0800

    Update botInfo.py

--- plugins/botInfo.py
+++ plugins/botInfo.py
@@ -3,8 +3,8 @@ from botsdk.tool.BotPlugin import BotPlugin
 class plugin(BotPlugin):
     def __init__(self):
         super().__init__()
-        self.name = "botInfo"
-        self.info = "bot来自XyzB0ts\n项目地址https://github.com/f88af65a/XyzB0ts"
+        self.name = "bot"
+        self.info = "来自XyzB0ts\n项目地址https://github.com/f88af65a/XyzB0ts"
         self.help = ""
 
 def handle(*args, **kwargs):

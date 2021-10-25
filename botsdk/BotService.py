33caba99c25284fd8f2503ec7df464f1af11c1cb
f88af65a <44595762+f88af65a@users.noreply.github.com>
2021-10-25 11:09:51 +0800

    update&bugfix

--- botsdk/BotService.py
+++ botsdk/BotService.py
@@ -11,19 +11,17 @@ class BotService:
     def __init__(self):
         pass
 
-    async def runInEventLoop(self, accountMark):
+    async def runInEventLoop(self, accountMark, concurrentModule):
         #初始化Bot
         self.bot = botsdk.Bot.Bot(getConfig()["account"][accountMark]["path"]
-            , getConfig()["account"][accountMark]["api"]["port"])
+            , getConfig()["account"][accountMark]["port"])
         #登录
         re = await self.bot.login(getConfig()["account"][accountMark]["qq"]
             , getConfig()["account"][accountMark]["passwd"])
         if re != 0:
             debugPrint(f'''账号{getConfig()["account"][accountMark]["qq"]}''', fromName="登录")
         #初始化BotRoute
-        self.botRoute = botsdk.BotRoute.BotRoute( self.bot, \
-            defaultBotConcurrentModule(int(getConfig()["workProcess"]) \
-            if getConfig()["multi"] else None, int(getConfig()["workThread"])))
+        self.botRoute = botsdk.BotRoute.BotRoute(self.bot, concurrentModule)
         self.botRoute.init()
         while True:
             re = await self.bot.fetchMessage(128)
@@ -42,6 +40,8 @@ class BotService:
     def run(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
-        for i in len(getConfig()["account"]):
-            asyncio.run_coroutine_threadsafe(self.runInEventLoop(i), self.loop)
+        concurrentModule = defaultBotConcurrentModule(int(getConfig()["workProcess"]) \
+            if getConfig()["multi"] else None, int(getConfig()["workThread"]))
+        for i in range(len(getConfig()["account"])):
+            asyncio.run_coroutine_threadsafe(self.runInEventLoop(i, concurrentModule), self.loop)
         self.loop.run_forever()

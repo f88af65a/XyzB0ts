6451e81178905673a24ff0d9c7130c113c8d81ec
f88af65a <44595762+f88af65a@users.noreply.github.com>
2021-11-02 21:58:42 +0800

    bugfix

--- plugins/random.py
+++ plugins/random.py
@@ -10,17 +10,17 @@ class plugin(BotPlugin):
         self.name = "random"
         self.addTarget("GroupMessage", "random", self.random)
     
-    def random(self, request: BotRequest):
+    async def random(self, request: BotRequest):
         "/random 最小值 最大值"
         data = request.getFirstTextSplit()
         if len(data) < 3:
-            request.sendMessage(MessageChain().plain(self.random.__doc__))
+            await request.sendMessage(MessageChain().plain(self.random.__doc__))
             return
         try:
             l, r = int(data[1]), int(data[2])
             if l > r:
                 raise
         except Exception:
-            request.sendMessage(MessageChain().plain(self.random.__doc__))
+            await request.sendMessage(MessageChain().plain(self.random.__doc__))
             return
-        request.sendMessage(MessageChain().plain(str(random.randint(l, r))))
+        await request.sendMessage(MessageChain().plain(str(random.randint(l, r))))

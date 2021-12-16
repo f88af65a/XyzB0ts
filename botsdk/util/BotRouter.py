4ae242eabfb5b0d3f945e6e8b63443d3ec24222d
f88af65a <44595762+f88af65a@users.noreply.github.com>
2021-12-16 10:51:17 +0800

    Update BotRouter.py
    使用了正则表达式

--- botsdk/util/BotRouter.py
+++ botsdk/util/BotRouter.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import re
 
 from botsdk.BotRequest import BotRequest
 from botsdk.util.BotConcurrentModule import defaultBotConcurrentModule
@@ -16,6 +17,10 @@ from botsdk.util.Permission import permissionCmp
 class BotRouter:
     def __init__(self):
         self.loop = asyncio.get_event_loop()
+        self.init()
+
+    def init(self):
+        pass
 
     async def route(self, pluginsManager: BotPluginsManager,
                     request: BotRequest,
@@ -28,7 +33,7 @@ class GeneralRouter(BotRouter):
                     request: BotRequest,
                     concurrentModule: defaultBotConcurrentModule = None):
         for i in pluginsManager.getGeneralList():
-            if (re := await i[1](request)) is not None and re is False:
+            if (ret := await i[1](request)) is not None and ret is False:
                 return False
             await asyncio.sleep(0)
         return True
@@ -47,38 +52,28 @@ class TypeRouter(BotRouter):
 
 
 class TargetRouter(BotRouter):
+    def init(self):
+        self.pattern = re.compile(
+            (r"^(\[(\S*?=\S*?)\])?(["
+             + "".join(["\\" + i for i in getConfig()["commandTarget"]])
+             + r"])(\S+)( \S+)*$"))
+
     async def route(self, pluginsManager: BotPluginsManager,
                     request: BotRequest,
                     concurrentModule: defaultBotConcurrentModule = None):
         if request.getType() not in messageType:
             return False
-        # 命令分析
+        # 命令提取
         target = request.getFirstTextSplit()
-        # 控制字段
-        controlData = {"size": 1}
-        if (target is not None and len(target) >= 2 and len(target[0]) > 2
-                and target[0][0] == "[" and target[0][-1] == "]"):
-            if not permissionCmp(
-                    str(getPermissionFromSystem(request.getSenderId())),
-                    "ADMINISTRATOR"):
-                await request.sendMessage(MessageChain().plain("使用控制字段权限不足"))
-                return
-            controlList = target[0][1: -1].split("&")
-            for i in controlList:
-                controlLineSplit = i.split("=")
-                if len(controlLineSplit) != 2:
-                    debugPrint("控制字段格式出错")
-                else:
-                    if controlLineSplit[0] == "size":
-                        controlData[controlLineSplit[0]] = (
-                            json.loads(controlLineSplit[1]))
-            del target[0]
-            request.getFirst("Plain")["text"] = " ".join(target)
-            request.setControlData(controlData)
-        # target获取
-        if target is None or len(target) == 0 or len(target[0]) == 0:
+        if target is None:
             return
-        target = target[0]
+        # 正则匹配
+        reData = self.pattern.search(target)
+        # target获取
+        if reData.group(4) is None:
+            return
+        target = reData.group(4)
+        # 判断target是否存在
         isTargetFlag = False
         for i in getConfig()["commandTarget"]:
             if len(target) >= len(i) and target[:len(i)] == i:
@@ -88,8 +83,28 @@ class TargetRouter(BotRouter):
         if not isTargetFlag:
             return
         request.setTarget(target)
+        # 控制字段判断
+        controlData = {"size": 1}
+        if reData.group(1) is not None:
+            # 控制字段权限判断
+            if not permissionCmp(
+                    str(getPermissionFromSystem(request.getSenderId())),
+                    "ADMINISTRATOR"):
+                await request.sendMessage(MessageChain().plain("使用控制字段权限不足"))
+                return
+            # 控制字段提取
+            controlList = reData.group(1).split("&")
+            for i in controlList:
+                controlLineSplit = i.split("=")
+                if len(controlLineSplit) != 2:
+                    debugPrint("控制字段格式出错")
+                else:
+                    if controlLineSplit[0] == "size":
+                        controlData[controlLineSplit[0]] = (
+                            json.loads(controlLineSplit[1]))
+            request.setControlData(controlData)
         # 命令判断
-        if (re := pluginsManager.getTarget(
+        if (ret := pluginsManager.getTarget(
                 request.getType(), target)) is not None:
             # 权限判断
             if not permissionCheck(request, target):
@@ -100,9 +115,10 @@ class TargetRouter(BotRouter):
                     request.getType(), target).__module__)
             # 路由
             for i in range(controlData["size"]):
-                if concurrentModule is not None and re.__self__.getCanDetach():
+                if (concurrentModule is not None
+                        and ret.__self__.getCanDetach()):
                     # 多进程方式
                     concurrentModule.addTask(request.getData())
                 else:
-                    await asyncHandlePacket(re, request)
+                    await asyncHandlePacket(ret, request)
         return True

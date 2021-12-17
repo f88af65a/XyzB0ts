a3f1cdb3a1c2a4851321167cab4f51ba52b117db
f88af65a <44595762+f88af65a@users.noreply.github.com>
2021-12-17 08:12:16 +0800

    update setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="XyzB0ts",
+    version="0.1",
+    url="https://github.com/f88af65a/XyzB0ts",
+    package_dir={"":"botsdk"},
+    packages=setuptools.find_packages(whrer="botsdk"),
+    requires=["redis"]
+)

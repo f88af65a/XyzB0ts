56b1d8a68dcc34a19e7102563fb5a78a744468ea
f88af65a <44595762+f88af65a@users.noreply.github.com>
2022-01-11 20:44:29 +0800

    PermissionUpdate bugfix#3

--- botsdk/util/Permission.py
+++ botsdk/util/Permission.py
@@ -35,7 +35,10 @@ def permissionCheck(request, target: str):
     cookie = cookie["permission"]
     m = 0
     while True:
-        if target in cookie and requestRole & set(cookie[target]):
+        permissionRoles = set(cookie[target])
+        if (target in cookie
+                and (requestRole & permissionRoles
+                     or "*" in permissionRoles)):
             return True
         if m < len(childs) and f":{childs[m]}" in cookie:
             cookie = cookie[f":{childs[m]}"]

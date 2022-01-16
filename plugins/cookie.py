35e35aa796457a269cebf046b9339a50d1e3f49d
f88af65a <44595762+f88af65a@users.noreply.github.com>
2022-01-16 16:56:05 +0800

    KaiheilaUpdate Update#1

--- plugins/cookie.py
+++ plugins/cookie.py
@@ -1,6 +1,7 @@
 import json
 
 from botsdk.util.BotPlugin import BotPlugin
+from botsdk.util.Cookie import getCookie, setCookie
 
 
 class plugin(BotPlugin):
@@ -12,6 +13,8 @@ class plugin(BotPlugin):
         self.addTarget("GROUP:1", "cookie", self.cookie)
         self.addTarget("GroupMessage", "id", self.id)
         self.addTarget("GROUP:1", "id", self.id)
+        self.addTarget("GroupMessage", "acookie", self.adminCookieControl)
+        self.addTarget("GROUP:1", "acookie", self.adminCookieControl)
         self.canDetach = True
 
     async def cookie(self, request):
@@ -21,6 +24,20 @@ class plugin(BotPlugin):
     async def id(self, request):
         await request.sendMessage(
             f"""{request.getId()} {request.getUserId()}""")
+
+    async def adminCookieControl(self, request):
+        "acookie ID [new cookie]"
+        data = request.getFirstText().split(" ")
+        if len(data) < 2:
+            request.sendMessage("缺少参数")
+        elif len(data) == 2:
+            request.sendMessage(getCookie(data[1]))
+        else:
+            try:
+                newCookie = data[2].split("=")
+                setCookie(data[1], newCookie[0], json.loads(newCookie[1]))
+            except Exception:
+                request.sendMessage("参数错误")
 
 
 def handle(*args, **kwargs):

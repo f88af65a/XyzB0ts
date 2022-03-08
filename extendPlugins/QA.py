3614c500f2b2e07d67b90de4926c7c4d295d007c
f88af65a <44595762+f88af65a@users.noreply.github.com>
2022-03-08 12:56:50 +0800

    Update QA.py

--- extendPlugins/QA.py
+++ extendPlugins/QA.py
@@ -1,3 +1,4 @@
+import json
 from botsdk.util.BotPlugin import BotPlugin
 
 
@@ -19,11 +20,17 @@ class handle(BotPlugin):
         keyWord = list(cookie.keys())
         for i in keyWord:
             if i in msg:
-                await request.sendMessage(cookie[i])
+                try:
+                    messageChain = json.loads(cookie[i])
+                    bot = request.getBot()
+                    groupid = request.getGroupId()
+                    await bot.sendGroupMessage(groupid, messageChain)
+                except Exception:
+                    await request.sendMessage(cookie[i])
                 break
 
     async def qaSet(self, request):
-        '''q&a [set/del/all] [关键字] [遇到关键字时触发的消息]'''
+        '''q&a [set/del/all/help] [关键字] [遇到关键字时触发的消息]'''
         data = request.getFirstTextSplit()
         if len(data) == 1:
             await request.sendMessage(self.qaSet.__doc__)
@@ -33,8 +40,30 @@ class handle(BotPlugin):
         cookie = request.getCookie("q&a")
         if cookie is None:
             cookie = dict()
-        if len(data) == 2 and data[1] == "all":
-            await request.sendMessage(",".join(list(cookie.keys())))
+        if len(data) == 2:
+            if (data[1] == "all" or data[1] == "help"):
+                await request.sendMessage(",".join(list(cookie.keys())))
+                return
+        elif len(data) == 3:
+            if data[1] == "set":
+                messageChain = request.getMessageChain()
+                for i in messageChain:
+                    if i["type"] == "Quote":
+                        bot = request.getBot()
+                        quoteMessageId = i["id"]
+                        quoteMessageChain = await bot.messageFromId(
+                            quoteMessageId)
+                        quoteMessageChain = (
+                            quoteMessageChain["data"]["messageChain"][1:])
+                        qaMessageChain = []
+                        for j in quoteMessageChain:
+                            qaMessageChain.append(j)
+                            if j["type"] == "Image":
+                                if "url" in qaMessageChain[j]:
+                                    del qaMessageChain[j]["url"]
+                cookie[data[2]] = json.dumps(qaMessageChain)
+                request.setCookie("q&a", cookie)
+                await request.sendMessage("设置成功")
         elif len(data) == 4 and data[1] == "set":
             cookie[data[2]] = data[3]
             request.setCookie("q&a", cookie)

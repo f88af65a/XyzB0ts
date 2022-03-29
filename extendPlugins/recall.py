1be759fd383a05e2b160fc9070014242f13bb86f
f88af65a <44595762+f88af65a@users.noreply.github.com>
2022-03-29 10:24:47 +0800

    Create recall.py

---
+++ extendPlugins/recall.py
@@ -0,0 +1,29 @@
+from botsdk.util.BotPlugin import BotPlugin
+
+
+class plugin(BotPlugin):
+    '''撤回消息'''
+    def onLoad(self):
+        self.addBotType("Mirai")
+        self.name = "recall"
+        self.addTarget("GroupMessage", "recall", self.recall)
+        self.canDetach = True
+
+    async def recall(self, request):
+        '''#recall 撤回一条消息'''
+        message = request.getMessageChain()
+        for i in message[1:]:
+            if i["type"] == "Quote":
+                bot = request.getBot()
+                quoteMessageId = i["id"]
+                try:
+                    await bot.recall(quoteMessageId)
+                    await request.sendMessage("撤回成功")
+                except Exception:
+                    await request.sendMessage("撤回失败")
+                return
+        await request.sendMessage("能回复吗，没有这个能力，再发下去，要输越南了")
+
+
+def handle(*args, **kwargs):
+    return plugin(*args, **kwargs)

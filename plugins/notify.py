77814939322e109bbef543348d00b31cac27e5e0
f88af65a <44595762+f88af65a@users.noreply.github.com>
2022-10-29 19:20:16 +0800

    1.0 update#7 bugfix#5

--- plugins/notify.py
+++ plugins/notify.py
@@ -12,33 +12,36 @@ class plugin(BotPlugin):
     async def manageNotify(self, request):
         "notify [add/remove/list] 通知名 #监听通知"
         data = request.getFirstTextSplit()
-        if len(data) < 3:
-            await request.sendMessage(self.manageNotify.__doc__)
-            return
-        cookie = getCookie("System:Notify")
-        if cookie is None:
-            cookie = {}
-        if data[2] not in cookie:
-            cookie[data[2]] = []
-        requestId = request.getId()
-        if data[1] == "add":
-            if requestId not in cookie[data[2]]:
-                cookie[data[2]].append(requestId)
-                setCookie("System:Notify", "NotifyList", cookie)
-            await request.sendMessage("修改完成")
-        elif data[1] == "remove":
-            if data[2] in cookie:
-                cookie.remove(data[2])
-                setCookie("System:Notify", "NotifyList", cookie)
-            await request.sendMessage("修改完成")
-        elif data[1] == "list":
-            haveList = []
-            for i in cookie:
-                if requestId in cookie:
-                    haveList.append(i)
-            await request.sendMessage(str(haveList))
-        else:
-            await request.sendMessage(self.manageNotify.__doc__)
+        if len(data) == 2:
+            cookie = getCookie("System:Notify")
+            if cookie is None:
+                cookie = {}
+            if data[2] not in cookie:
+                cookie[data[2]] = []
+            requestId = request.getId()
+            if data[1] == "list":
+                haveList = []
+                for i in cookie:
+                    if requestId in cookie:
+                        haveList.append(i)
+                await request.sendMessage(str(haveList))
+        elif len(data) == 3:
+            cookie = getCookie("System:Notify")
+            if cookie is None:
+                cookie = {}
+            if data[2] not in cookie:
+                cookie[data[2]] = []
+            requestId = request.getId()
+            if data[1] == "add":
+                if requestId not in cookie[data[2]]:
+                    cookie[data[2]].append(requestId)
+                    setCookie("System:Notify", "NotifyList", cookie)
+                await request.sendMessage("修改完成")
+            elif data[1] == "remove":
+                if data[2] in cookie:
+                    cookie.remove(data[2])
+                    setCookie("System:Notify", "NotifyList", cookie)
+                await request.sendMessage("修改完成")
 
 
 def handle():

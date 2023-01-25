0fd935cd08830bd3432d2cadc13e01ffe38c1941
f88af65a <44595762+f88af65a@users.noreply.github.com>
2023-01-25 14:47:51 +0800

    1.0 update#43 bugfix#5

--- extendPlugins/bfv.py
+++ extendPlugins/bfv.py
@@ -12,6 +12,8 @@ class plugin(BotPlugin):
         self.addBotType("Kaiheila")
         self.addTarget("GroupMessage", "bfv.search", self.search)
         self.addTarget("GROUP:9", "bfv.search", self.search)
+        self.addTarget("GroupMessage", "bfv.server.search", self.serverSearch)
+        self.addTarget("GROUP:9", "bfv.server.search", self.serverSearch)
 
     async def search(self, request):
         '''bfv.search id #查询战绩'''
@@ -52,6 +54,45 @@ class plugin(BotPlugin):
             f'''最高连杀: {response["highestKillStreak"]}\n'''
             f'''游戏时间: {response["timePlayed"]}'''
         )
+
+    async def serverSearch(self, request):
+        '''bfv.server.search 关键字 #查询战绩'''
+        data = request.getFirstTextSplit()
+        if len(data) != 2:
+            await request.send(self.search.__doc__)
+            return
+        response = await get(
+            "https://api.gametools.network/bfv/servers/?"
+            f"name={data[1]}&region=all&platform=pc&limit=10&lang=en-us"
+        )
+        if response is None:
+            await request.send(f"获取{data[1]}的数据失败")
+            return
+        try:
+            response = loads(response)
+        except Exception:
+            await request.send(f"解析{data[1]}的数据失败")
+            return
+        if "errors" in response:
+            await request.send(
+                "\n".join(response["errors"])
+            )
+            return
+        if "servers" not in response:
+            await request.send(
+                "返回格式有误"
+            )
+            return
+        retMsg = []
+        for i in response["servers"]:
+            retMsg.append(
+                f'''{i["prefix"]}\n'''
+                f'''人数: {i["playerAmount"]}/{i["maxPlayers"]}'''
+                f''' (+{i["inQue"]})\n'''
+                f'''地图: {i["mode"]}\n'''
+                f'''模式: {i["currentMap"]}'''
+            )
+        await request.send("搜到以下服务器\n" + "\n".join(retMsg))
 
 
 def handle(*args, **kwargs):
